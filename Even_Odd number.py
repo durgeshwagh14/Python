f66e74da675f494586a326a58d2b34f56a017a08
Durgesh Wagh <durgeshwagh1405@gmail.com>
2025-07-16 11:46:27 +0530

    Update Even_Odd number.py

--- Even_Odd number.py
+++ Even_Odd number.py
@@ -1,20 +1,26 @@
-numbers = []
-print("Enter 10 numbers:")
-for i in range(10):
-    num_str = input(f"Enter number {i+1}: ")
-    num = int(num_str)
-    numbers.append(num)
+user = int(input("How many number you want to enter ?"))
 
-even_numbers = []
+even_number=[]
+odd_number=[]
 
-odd_numbers = []
+for i in range(user):
+  num = int(input(f"Enter a number {i+1} :"))
 
-for number in numbers:
-    if number % 2 == 0:
-        even_numbers.append(number)
-    else:
-        odd_numbers.append(number)
+  if num % 2 == 0:
+    even_number.append(num)
+  else:
+    odd_number.append(num)
 
-print("\nAll entered numbers:", numbers)
-print("Even numbers:", even_numbers)
-print("Odd numbers:", odd_numbers)
+print()
+print("The Even Number is :",*even_number)
+print("The Sum of Even Number is : ",sum(even_number))
+even_number.sort()
+print("The Even Number Sort in Acending Order :",*even_number)
+print()
+print("The odd Number is : ",*odd_number)
+print("The Sum of Odd Number is : ",sum(odd_number))
+odd_number.sort()
+print("The Odd Number Sort in Acending Order :",*odd_number)
+print()
+print("The Even count is :",len(even_number))
+print("The Odd count is :",len(odd_number))
