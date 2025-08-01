4faabf1248357afa8cd87a214f934d2f4ccd75c8
Durgesh Wagh <durgeshwagh1405@gmail.com>
2025-08-01 12:09:04 +0530

    Create polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,23 @@
+class Shape:
+    def area(self):
+        pass
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+class Square(Shape):
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+def get_area(shape):
+    print(f"The area is: {shape.area()}")
+
+get_area(Circle(5))
+get_area(Square(4))
